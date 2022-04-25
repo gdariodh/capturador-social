ceb1528bdc0a87f34b434b6786d0c5ce658d8982
gdariodh <diazhernandezgabriel2001@gmail.com>
2022-04-24 22:20:17 -0400

    read user in login js

--- login.js
+++ login.js
@@ -1,44 +1,14 @@
-let user = "";
-let password = "";
 const form = document.querySelector("#form");
-let errors = {}
-let userData = {}
 
-function obtenerDatos(){
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
- user = document.getElementById("user").value.toLowerCase();
- password = document.getElementById("password").value;
+  user = document.getElementById("user").value.toLowerCase();
+  password = document.getElementById("password").value.toLowerCase();
 
- if(user != "admin"){
-    errors.user = "El usuario ingresado es incorrecto"
- }
-
- console.log(password)
-
- if(password != "000111"){
-     errors.password = "El password es incorrecto"
- } 
-
-console.log(errors)
-
-console.log(Object.keys(errors).length)
-    
-}
-
-var input = document.getElementsByTagName("INPUT");
-
-for (i=0; i<input.length; i++) {
- input[i].addEventListener("change",  function(){
-  resultados = obtenerDatos();
-  console.log(resultados);
- });
-};
-
-
-form.addEventListener("submit", () => {
-
-   
-
-    window.location.replace("prinicipal.html")
-    console.log("enviando...")
-})
+  if (user === "admin" && password === "000111") {
+    window.location.assign("./principal.html");
+  } else {
+    alert("El usuario o la password estan incorrectos");
+  }
+});
